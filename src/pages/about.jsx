e1b552f6666508b51b0ba6dfa96bc12333a3f595
Bruce Doby <dobybruce@gmail.com>
2025-02-26 18:37:50 +0100

    mise à jour des dropdowns

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,7 +8,9 @@ function About() {
     <div className='land__div2'>
       <img src={landBackground2} alt='photo de paysage' className='land__img2' />
     </div>
-    <Dropdown />
+    <Dropdown title='Fiabilité' content='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.' />
+    <Dropdown title='Fiabilité' content='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.' />
+
     {/*<div className='dropdowns'>
       <div className='dropdown'>
         <p>Fiabilité</p>
