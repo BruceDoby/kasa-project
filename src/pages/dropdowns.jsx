b45baaec2597faaa4cdcf3540f7e5afafd9bf203
Bruce Doby <dobybruce@gmail.com>
2025-03-07 11:46:48 +0100

    centralisation des style du dropdown et correction de la position du chevron

--- src/pages/dropdowns.jsx
+++ src/pages/dropdowns.jsx
@@ -10,6 +10,7 @@ const Dropdown = (props) => {
   const [openIndex, setOpenIndex] = useState(null); 
 
   const toggleDropdown = (index) => {
+    console.log("test", openIndex)
     if (openIndex === index) {
       setOpenIndex(null); 
     } else {
